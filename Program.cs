fcdafa8c96a6feba1e6290846b740b09df1eb821
«Daria» <Spirina_0136@mail.ru>
2023-10-02 23:03:05 +0700

    Commit2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трезначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number >=100 && number <1000)
+{
+    int mid = ((number / 10) % 10);
+    Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+}
+else 
+{
+    Console.WriteLine($"Это число не трехзначное");
+}
